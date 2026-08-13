a515d2f643b34116d914b5909a422312f901248a
Karthikeya Matta <mattakarthikeya123@gmail.com>
2026-08-13 19:54:36 +0530

    Update CORS configuration to allow OPTIONS method

--- backend/src/main/java/com/resumelens/config/WebConfig.java
+++ backend/src/main/java/com/resumelens/config/WebConfig.java
@@ -6,14 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
-    "http://localhost:5173",
-    "https://resume-lens-omega.vercel.app"
-)
-                .allowedMethods("GET", "POST", "DELETE")
+                        "http://localhost:5173",
+                        "https://resume-lens-omega.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type")
                 .maxAge(3600);
     }
